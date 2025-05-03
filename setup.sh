c9944f29c85666b8a7d22df6eff80ddc32db8e5d
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2025-05-02 22:58:06 -0700

    fix path for chmod

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ cd ..
 SPLUNK_SCRIPTS_PATH=`pwd`
 echo "source $GIT_PATH/venv/bin/activate" > ../api_poller.sh
 echo "python3 $GIT_PATH/scripts/api_poller.py $SPLUNK_SCRIPTS_PATH/settings.json" >> ../api_poller.sh
-chmod +x ../api_poller.sh
+chmod +x $SPLUNK_SCRIPTS_PATH/api_poller.sh
 
 echo "================================"
 echo "Copy the following to your inputs.conf file"
